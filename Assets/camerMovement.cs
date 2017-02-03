9bcf9e7cf072d49f12250ccef3214f04a4d5de4e
Hamid Yoqoub <hamidyoqoub@gmail.com>
2017-02-03 15:21:04 -0500

    3rd player camera

---
+++ Assets/camerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camerMovement : MonoBehaviour {
+
+	public Transform lookAt;
+	public Transform camTransform;
+
+	private Camera cam;
+
+	private float distance = 10.0f;
+	private float currentX = 0.0f;
+	private float currentY = 0.0f;
+	private float sensivityX = 4.0f;
+	private float sensivityy = 1.0f;
+
+	private void Start(){
+		camTransform = transform;
+		cam = Camera.main;
+
+	}
+
+
+
+
+	private void LateUpdate(){
+		Vector3 dir = new Vector3 (0, 0, - distance);
+		Quaternion rotation = Quaternion.Euler(currentY, currentX,0);
+		camTransform.position = lookAt.position + rotation * dir;
+		camTransform.LookAt (lookAt.position);
+	}
+		
+}
