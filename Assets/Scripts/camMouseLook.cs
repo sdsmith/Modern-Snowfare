5296a397553bb61f4c93da5dfa9be54131bdb210
Hamid Yoqoub <hamidyoqoub@gmail.com>
2017-02-03 20:23:26 -0500

    Implemented Shooting

--- Assets/Scripts/camMouseLook.cs
+++ Assets/Scripts/camMouseLook.cs
@@ -18,12 +18,12 @@ public class camMouseLook : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		var md = new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y"));
+		var md = new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y")	);
 		md = Vector2.Scale (md, new Vector2 (sensitivity * smoothing, sensitivity * smoothing));
 		smoothV.x = Mathf.Lerp (smoothV.x, md.x, 1f / smoothing);
 		smoothV.y = Mathf.Lerp (smoothV.y, md.y, 1f / smoothing);
 		mouseLook += smoothV;
-		//mouseLook.y = Mathf.Clamp (mouseLook.y, -90f, 90f);
+		mouseLook.y = Mathf.Clamp (mouseLook.y, -10f, 45f);
 
 		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
 		character.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, character.transform.up);
--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class characterController : MonoBehaviour {
 
 	public float speed= 10.0F;
+	public weaponController theWeapon;
 
 	// Use this for initialization
 	void Start () {
@@ -21,5 +22,12 @@ public class characterController : MonoBehaviour {
 
 		if (Input.GetKeyDown ("escape"))
 			Cursor.lockState = CursorLockMode.None;
+		//0 left click
+		if (Input.GetMouseButtonDown (0)) {
+			theWeapon.isFiring = true;
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			theWeapon.isFiring = false;
+		}
 	}
 }
