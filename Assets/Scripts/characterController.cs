2b23f151b82aa2e66b65b4edfd9cf5506e8a9aed
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2017-02-08 17:49:34 -0500

    Added CharacterController and ported movement code to use it. Added gravity.

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(CharacterController))]
 public class characterController : MonoBehaviour {
 
-	public float speed= 10.0F;
+	public float speed = 10.0F;
+    public float gravity = 9.81F;
 	public weaponController theWeapon;
 
 	// NOTE: Use this for initialization
@@ -14,14 +16,23 @@ public class characterController : MonoBehaviour {
 	
 	// NOTE: Update is called once per frame
 	void Update () {
-        // Character object translation
-		float translation = Input.GetAxis ("Vertical") * speed;
-		float strafe = Input.GetAxis ("Horizontal") * speed;
-		strafe *= Time.deltaTime;
-		translation *= Time.deltaTime;
+        {
+            // Character movement
+            CharacterController controller = GetComponent<CharacterController>();
+            float forwardMove = Input.GetAxis("Vertical");
+            float sideMove = Input.GetAxis("Horizontal");
+            Vector3 moveDirection = new Vector3(sideMove, 0, forwardMove);
 
-		transform.Translate (strafe, 0, translation);
-        
+            // Transform from Local to World space
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            moveDirection *= speed;
+            moveDirection.y -= gravity * Time.deltaTime; // m/s
+
+            // Move
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+
 
         // TODO(sdsmith): Refactor non-character specific input out of here.
 		if (Input.GetKeyDown ("escape"))
