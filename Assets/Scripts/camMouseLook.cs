9f2294d4138c6655ad4b085ecd0d257d0446c242
Ryan Llewellin <ryanllewellin@icloud.com>
2017-02-08 17:57:23 -0500

    Added map

--- Assets/Scripts/camMouseLook.cs
+++ Assets/Scripts/camMouseLook.cs
@@ -7,6 +7,10 @@ public class camMouseLook : MonoBehaviour {
 	Vector2 smoothV;
 	public float sensitivity = 5.0f;
 	public float smoothing = 2.0f;
+
+	public float clampMin = -45f;
+	public float clampMax = 45f;
+
 	GameObject character;
 
 
@@ -23,7 +27,7 @@ public class camMouseLook : MonoBehaviour {
 		smoothV.x = Mathf.Lerp (smoothV.x, md.x, 1f / smoothing);
 		smoothV.y = Mathf.Lerp (smoothV.y, md.y, 1f / smoothing);
 		mouseLook += smoothV;
-		mouseLook.y = Mathf.Clamp (mouseLook.y, -10f, 45f);
+		mouseLook.y = Mathf.Clamp (mouseLook.y, clampMin, clampMax);
 
 		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
 		character.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, character.transform.up);
