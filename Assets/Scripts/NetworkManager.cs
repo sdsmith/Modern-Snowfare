ae86503703b79c43854c4d3f396917897c3f1aa5
Ryan Llewellin <ryanllewellin@icloud.com>
2017-02-09 21:50:55 -0500

    Multiplayer FPS Tutorial 5/19

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+	SpawnSpot[] spawnSpots;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
+		Connect ();
+	}
+
+	void Connect() {
+		Debug.Log ("Connect");
+		PhotonNetwork.ConnectUsingSettings( "MultiFPS v001" );
+
+	}
+
+	void OnGUI() {
+		GUILayout.Label( PhotonNetwork.connectionStateDetailed.ToString() );
+	}
+
+	void OnJoinedLobby() {
+		Debug.Log ("OnJoinedLobby");
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed() {
+		Debug.Log ("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom( null );
+	}
+
+	void OnJoinedRoom() {
+		Debug.Log ("OnJoinedRoom");
+
+		SpawnMyPlayer();
+	}
+
+	void SpawnMyPlayer() {
+		if (spawnSpots == null) {
+			Debug.Log ("SpawnMyPlayer: SpawnSpots == null");
+			return;
+		}
+		SpawnSpot mySpawnSpot = spawnSpots [Random.Range (0, spawnSpots.Length)];
+		GameObject myPlayerGO = (GameObject)PhotonNetwork.Instantiate ("Player", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 0);
+		myPlayerGO.GetComponent<characterController> ().enabled = true;
+		// myPlayerGO.GetComponentInChildren<camMouseLook> ().enabled = true;
+		myPlayerGO.transform.FindChild("Main Camera").gameObject.SetActive(true);
+	}
+
+}
