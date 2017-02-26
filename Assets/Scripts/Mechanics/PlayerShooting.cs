5b5757e8fb08a56f515ab747131cce631d0e66f1
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2017-02-26 15:47:32 -0500

    Hotfix for shooting scene objects (ie. they have no set team).

--- Assets/Scripts/Mechanics/PlayerShooting.cs
+++ Assets/Scripts/Mechanics/PlayerShooting.cs
@@ -4,109 +4,122 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour {
 
-	public float fireRate = 0.5f;
-	float coolDown = 0;
-	public float damage = 25f;
-	FXManager fxManager;
+    public float fireRate = 0.5f;
+    float coolDown = 0;
+    public float damage = 25f;
+    FXManager fxManager;
 
-	void Start(){
-		fxManager = GameObject.FindObjectOfType<FXManager> ();
+    void Start() {
+        fxManager = GameObject.FindObjectOfType<FXManager>();
 
-		if (fxManager == null) {
-			Debug.LogError ("No fxManager");
-		}
-	}
+        if (fxManager == null) {
+            Debug.LogError("No fxManager");
+        }
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update() {
 
-		coolDown -= Time.deltaTime;
+        coolDown -= Time.deltaTime;
 
-		if(Input.GetButton("Fire1")){
-			Fire ();
-		}
-	}
+        if (Input.GetButton("Fire1")) {
+            Fire();
+        }
+    }
 
-	void Fire(){
-		if (coolDown > 0) {
-			return;
-		}
+    void Fire() {
+        if (coolDown > 0) {
+            return;
+        }
 
-		// Debug.Log ("firing gun");
+        // Debug.Log ("firing gun");
 
-		Ray ray = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
-		Transform hitTransform;
-		Vector3 hitPoint;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        Transform hitTransform;
+        Vector3 hitPoint;
 
-		hitTransform = FindClosestHitObject (ray, out hitPoint);
+        hitTransform = FindClosestHitObject(ray, out hitPoint);
 
-		// If we hit something, lets resolve the hit
-		if (hitTransform != null) {
-			Debug.Log ("HIT: " + hitTransform.name);
-			Health h = hitTransform.GetComponent<Health> ();
+        // If we hit something, lets resolve the hit
+        if (hitTransform != null) {
+            Debug.Log("HIT: " + hitTransform.name);
+            Health h = hitTransform.GetComponent<Health>();
 
-			// check if the things parent has a Health object
-			while (h == null && hitTransform.parent) {
-				hitTransform = hitTransform.parent;
-				h = hitTransform.GetComponent<Health> ();
-			}
+            // check if the things parent has a Health object
+            while (h == null && hitTransform.parent) {
+                hitTransform = hitTransform.parent;
+                h = hitTransform.GetComponent<Health>();
+            }
 
-			if (h != null) {
-				// TODO h.resolve(damage);
-				// h.TakeDamage (damage);
-				// next line equivalent of calling h.takeDamage except over network
-				PhotonView pv = h.GetComponent<PhotonView> ();
-				if (pv == null) {
-					Debug.Log ("PlayerShooting: PhotonView is null");
-				} else {
+            if (h != null) {
+                // TODO h.resolve(damage);
+                // h.TakeDamage (damage);
+                // next line equivalent of calling h.takeDamage except over network
+                PhotonView pv = h.GetComponent<PhotonView>();
+                if (pv == null) {
+                    Debug.Log("PlayerShooting: PhotonView is null");
+                } else {
 
-					//get the player that was hit
-					PhotonPlayer target = pv.owner;
+                    // get the object that was hit
+                    PhotonPlayer target = pv.owner;
 
-					if (PhotonNetwork.player.GetTeam () != target.GetTeam ()) {
-						pv.RPC ("TakeDamage", PhotonTargets.AllBuffered, damage);
-						// Debug.Log ("Teams don't match, take damage");
-					} else {
-						// Debug.Log ("FRIENDLY FIRE STAHP IT");
-					}
-				}
-			}
+                    // Get teams
+                    PunTeams.Team ourTeam = PhotonNetwork.player.GetTeam();
+                    PunTeams.Team theirTeam = PunTeams.Team.none; // default objects to no team
 
-			if (fxManager != null) {
-				fxManager.GetComponent<PhotonView> ().RPC ("SnowballFX", PhotonTargets.All, Camera.main.transform.position, hitPoint);
-			}
+                    // Check if the target object has an owner
+                    // @NOTE(sdsmith): PhotonView.owner is null for scene objects
+                    // https://doc-api.photonengine.com/en/pun/current/class_photon_view.html#ad696cb93fb9835d633b9def970650edc
+                    if (target != null) {
+                        // Target has an owner (not a scene object), set its team
+                        theirTeam = target.GetTeam();
+                    }
 
-		} 
-		else {
-			// Didn't hit anything (maybe empty space) but we want a visual fx anyway
-			if (fxManager != null) {
-				hitPoint = Camera.main.transform.position + (Camera.main.transform.forward * 100f);
-				fxManager.GetComponent<PhotonView> ().RPC ("SnowballFX", PhotonTargets.All, Camera.main.transform.position, hitPoint);
-			}
-		}
+                    if (ourTeam != theirTeam) {
+                        // Not targeting same team
+                        pv.RPC("TakeDamage", PhotonTargets.AllBuffered, damage);
+                        // Debug.Log ("Teams don't match, take damage");
+                    } else {
+                        // Targeting same team
+                        // Debug.Log ("FRIENDLY FIRE STAHP IT");
+                    }
+                }
+            }
 
-		coolDown = fireRate;
-		
-	}
+            if (fxManager != null) {
+                fxManager.GetComponent<PhotonView>().RPC("SnowballFX", PhotonTargets.All, Camera.main.transform.position, hitPoint);
+            }
 
-	Transform FindClosestHitObject(Ray ray, out Vector3 hitPoint){
+        } else {
+            // Didn't hit anything (maybe empty space) but we want a visual fx anyway
+            if (fxManager != null) {
+                hitPoint = Camera.main.transform.position + (Camera.main.transform.forward * 100f);
+                fxManager.GetComponent<PhotonView>().RPC("SnowballFX", PhotonTargets.All, Camera.main.transform.position, hitPoint);
+            }
+        }
 
-		RaycastHit[] hits = Physics.RaycastAll (ray);
+        coolDown = fireRate;
 
-		Transform closestHit = null;
-		float distance = 0;
-		hitPoint = Vector3.zero;
+    }
 
-		foreach (RaycastHit hit in hits) {
-			// if we don't hit ourselves & its the closest thing to us
-			if(hit.transform != this.transform && (closestHit == null || hit.distance < distance)){
-				closestHit = hit.transform;
-				distance = hit.distance;
-				hitPoint = hit.point;
-			}
-		}
+    Transform FindClosestHitObject(Ray ray, out Vector3 hitPoint) {
 
-		//return the closest hit object or null if nothing was hit
-		return closestHit;
-	}
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+
+        Transform closestHit = null;
+        float distance = 0;
+        hitPoint = Vector3.zero;
+
+        foreach (RaycastHit hit in hits) {
+            // if we don't hit ourselves & its the closest thing to us
+            if (hit.transform != this.transform && (closestHit == null || hit.distance < distance)) {
+                closestHit = hit.transform;
+                distance = hit.distance;
+                hitPoint = hit.point;
+            }
+        }
+
+        //return the closest hit object or null if nothing was hit
+        return closestHit;
+    }
 }
