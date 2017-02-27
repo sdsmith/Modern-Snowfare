98b5b5e50390c5353f5861090c94c2fa44b661e5
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2017-02-26 20:25:21 -0500

    Fixed crazy snowballs.

--- Assets/Scripts/Controller/SnowballController.cs
+++ Assets/Scripts/Controller/SnowballController.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
+[RequireComponent(typeof(Rigidbody))]
 public class SnowballController : MonoBehaviour {
 
-	public float speed = 10f;
-	// Use this for initialization
+	public float speed = 150f;// m/s(?)
+
+
+
 	void Start () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        // Apply initial forward velocity to the snowball at creation and let 
+        // the physics engine do its work.
+        rb.AddRelativeForce(Vector3.forward * speed, ForceMode.VelocityChange);
+    }
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.forward * speed * Time.deltaTime);
 	}
 	
 
