9cf10332625688a0642a129660fecddc7e5ba613
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2017-02-27 14:13:39 -0500

    Removed the enabled attr from DebugOverlay (can enable/disable component). Added overlay to _SCRIPTS.

--- Assets/Scripts/DebugOverlay.cs
+++ Assets/Scripts/DebugOverlay.cs
@@ -6,12 +6,11 @@ using UnityEngine;
 
 public class DebugOverlay : MonoBehaviour {
 
-    public bool enabled = true;
     private List<KeyValuePair<string,string>> attributes;
 
 
 
-	void Start () {
+    void Start() {
         attributes = new List<KeyValuePair<string, string>>();
     }
 
@@ -19,27 +18,23 @@ public class DebugOverlay : MonoBehaviour {
         attributes.Add(new KeyValuePair<string, string>(key, value));
     }
 
-	void Update () {
-        if (enabled) {
-            attributes.Clear();
+    void Update() {
+        attributes.Clear();
 
-            if (Camera.main) {
-                AddAttr("player pos", Camera.main.transform.position.ToString());
-                AddAttr("player fwd", Camera.main.transform.forward.ToString());
-            }
+        if (Camera.main) {
+            AddAttr("player pos", Camera.main.transform.position.ToString());
+            AddAttr("player fwd", Camera.main.transform.forward.ToString());
         }
-	}
+    }
 
 
     void OnGUI() {
-        if (enabled) {
-            string text = "";
+        string text = "";
 
-            foreach (KeyValuePair<string, string> attr in attributes) {
-                text += attr.Key + ": \t" + attr.Value + "\n";
-            }
-
-            GUI.TextArea(new Rect(10, 10, Screen.width / 3 - 10, Screen.height - 10), text);
+        foreach (KeyValuePair<string, string> attr in attributes) {
+            text += attr.Key + ": \t" + attr.Value + "\n";
         }
+
+        GUI.TextArea(new Rect(10, 10, Screen.width / 3 - 10, Screen.height - 10), text);
     }
 }
