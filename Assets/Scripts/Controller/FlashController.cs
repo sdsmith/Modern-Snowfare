8bdacf638c2b105b6bc00d6203cce7fcf9a69281
Ryan Llewellin <ryanllewellin@icloud.com>
2017-03-04 15:39:39 -0500

    Added sniper controller and refactored flash and juggernaut code

--- Assets/Scripts/Controller/FlashController.cs
+++ Assets/Scripts/Controller/FlashController.cs
@@ -3,20 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlashController : PlayerController {
-	/*
-	 * @NOTE(Llewellin): Character controllers inherit from PlayerController.
-	 * If you're overriding a parent method, check to see if you should call
-	 * the parent function first with base.<function>(); Ensure the function
-	 * is public/protected, Unity defaults to private.
-	 */
 
-	// Use this for initialization
-	void Start () {
-		base.Start ();
-
-		speed *= 2;
-
-		// @DEBUG(Llewellin): Add entry to debug overlay
-		DebugOverlay.AddAttr("speed", speed.ToString());
+	public override float GetSpeed () {
+		return base.GetSpeed() * 2;
 	}
 }
--- Assets/Scripts/Controller/JuggernautController.cs
+++ Assets/Scripts/Controller/JuggernautController.cs
@@ -3,17 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JuggernautController : PlayerController {
-	/*
-	 * @NOTE(Llewellin): Character controllers inherit from PlayerController.
-	 * If you're overriding a parent method, check to see if you should call
-	 * the parent function first with base.<function>(); Ensure the function
-	 * is public/protected, Unity defaults to private.
-	 */
 
-	// Use this for initialization
-	void Start () {
-		base.Start ();
-
-		hitPoints *= 2; 
+	public override float GetHealth (){
+		return base.GetHealth () * 2;
 	}
 }
