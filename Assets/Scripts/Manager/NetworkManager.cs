adcb0717104b49b77bc9829e9c868f099802577e
Mansi Vaghela <mansipv@gmail.com>
2017-03-13 14:59:29 -0400

    can pick 4 characters and everything looks okay now

--- Assets/Scripts/Manager/NetworkManager.cs
+++ Assets/Scripts/Manager/NetworkManager.cs
@@ -132,7 +132,7 @@ public class NetworkManager : MonoBehaviour {
                 {
                     GUILayout.BeginHorizontal();
                     {
-                        if (GUILayout.Button("healer", m_PickButtonStyle, GUILayout.Width(Screen.width * 0.25f - 20), GUILayout.Height(Screen.height - 140)))
+                        if (GUILayout.Button("healer", m_PickButtonStyle, GUILayout.Width(Screen.width * 0.25f - 20), GUILayout.Height(Screen.height - 20)))
                         {
                             prefabName = "Healer";
                             hasPickedChar = true;
@@ -140,19 +140,19 @@ public class NetworkManager : MonoBehaviour {
 
                         GUILayout.FlexibleSpace();
 
-                        if (GUILayout.Button("flash", m_PickButtonStyle, GUILayout.Width(Screen.width * 0.25f - 20), GUILayout.Height(Screen.height - 140)))
+                        if (GUILayout.Button("flash", m_PickButtonStyle, GUILayout.Width(Screen.width * 0.25f), GUILayout.Height(Screen.height - 20)))
                         {
                             prefabName = "Flash";
                             hasPickedChar = true;
                         }
 
-                        if (GUILayout.Button("juggernaut", m_PickButtonStyle, GUILayout.Width(Screen.width * 0.25f - 20), GUILayout.Height(Screen.height - 140)))
+                        if (GUILayout.Button("tank", m_PickButtonStyle, GUILayout.Width(Screen.width * 0.25f), GUILayout.Height(Screen.height - 20)))
                         {
                             prefabName = "Juggernaut";
                             hasPickedChar = true;
                         }
 
-                        if (GUILayout.Button("sniper", m_PickButtonStyle, GUILayout.Width(Screen.width * 0.25f - 20), GUILayout.Height(Screen.height - 140)))
+                        if (GUILayout.Button("sniper", m_PickButtonStyle, GUILayout.Width(Screen.width * 0.25f), GUILayout.Height(Screen.height - 20)))
                         {
                             prefabName = "Sniper";
                             hasPickedChar = true;
