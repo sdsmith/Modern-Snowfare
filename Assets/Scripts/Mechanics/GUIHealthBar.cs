f32fe76324a5f6b6e9cdf7a884772c6a20c58b6f
Mansi Vaghela <mansipv@gmail.com>
2017-03-27 18:35:11 -0400

    moved the health bar and changed the size, displays percents now

--- Assets/Scripts/Mechanics/GUIHealthBar.cs
+++ Assets/Scripts/Mechanics/GUIHealthBar.cs
@@ -17,12 +17,12 @@ public class GUIHealthBar : MonoBehaviour {
 	void Start() {
         health = GetComponent<Health>();
 
-        size = new Vector2(Mathf.Min(Screen.width / 3.0f, 500), Mathf.Min(Screen.height / 10.0f, 50));
-        position = new Vector2(Screen.width / 2.0f - size.x / 2.0f, Screen.height - size.y);
+        size = new Vector2(Mathf.Min(Screen.width / 3.0f, 500), Mathf.Min(Screen.height / 10.0f, 25));
+        position = new Vector2(Screen.width - size.x, Screen.height - size.y);
 
         // Create health bar colours
         backgroundTex = new Texture2D(1, 1);
-        backgroundTex.SetPixel(0, 0, Color.gray);
+        backgroundTex.SetPixel(0, 0, Color.red);
         backgroundTex.wrapMode = TextureWrapMode.Repeat;
         backgroundTex.Apply();
         foregroundTex = new Texture2D(1, 1);
@@ -34,16 +34,18 @@ public class GUIHealthBar : MonoBehaviour {
 	void OnGUI() {
         if (gameObject != null && gameObject == Util.localPlayer) {
             float fillPercent = health.GetCurrentHitPoints() / health.GetMaxHitPoints();
-
+            GUIContent content = new GUIContent();
+            content.text = (fillPercent * 100) + "%";
+            GUI.contentColor = Color.black;
             // Draw the health bar
             {
                 GUI.BeginGroup(new Rect(position.x, position.y, size.x, size.y));
                 GUI.skin.box.normal.background = backgroundTex;
-                GUI.Box(new Rect(0, 0, size.x, size.y), GUIContent.none);
+                GUI.Box(new Rect(0, 0, size.x, size.y), content);
                 {
                     GUI.BeginGroup(new Rect(0, 0, size.x * fillPercent, size.y));
                     GUI.skin.box.normal.background = foregroundTex;
-                    GUI.Box(new Rect(0, 0, size.x, size.y), GUIContent.none);
+                    GUI.Box(new Rect(0, 0, size.x, size.y), content);
                     GUI.EndGroup();
                 }
                 GUI.EndGroup();
