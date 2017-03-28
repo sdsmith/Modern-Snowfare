24278c3191949fcde750898adb8c1b6ff1ab7ebc
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2017-03-28 02:02:19 -0400

    Spawn points are now rotated toward the center of the map.

---
+++ Assets/Scripts/Controller/SpawnPointController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointController : MonoBehaviour {
+    
+	void Start () {
+        // Rotate spawn point toward the center of the map
+        {
+            // Get the terrain center point
+            Terrain terrain = GameObject.FindGameObjectWithTag("Terrain").GetComponent<Terrain>();
+            Vector3 terrainSize = terrain.terrainData.size;
+            Vector3 centerPoint = terrain.transform.position + terrainSize / 2f;
+
+            // Create rotation
+            Vector3 toCenter = centerPoint - transform.position;
+            toCenter.y = 0; // y doesn't matter
+            Quaternion rotToCenter = Quaternion.FromToRotation(transform.forward, toCenter);
+
+            // Rotate
+            transform.rotation = rotToCenter * transform.rotation;
+        }
+    }
+}
