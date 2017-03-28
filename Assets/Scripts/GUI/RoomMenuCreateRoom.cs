444ce4c06bbe6e8634d66610f64f4759a121931a
Mansi Vaghela <mansipv@gmail.com>
2017-03-28 03:41:59 -0400

    created label for tutorial video link

--- Assets/Scripts/GUI/RoomMenuCreateRoom.cs
+++ Assets/Scripts/GUI/RoomMenuCreateRoom.cs
@@ -20,6 +20,10 @@ public class RoomMenuCreateRoom : RoomMenuBase
 
     void DrawVideo()
     {
-
-    }
+        GUILayout.BeginArea(new Rect(Screen.width / 2.0f, Screen.height / 2.0f, Screen.width, Screen.height));
+        {
+            GUILayout.Label("Insert some youtube link", Styles.Header);
+        }
+        GUILayout.EndArea();
+        }
 }
